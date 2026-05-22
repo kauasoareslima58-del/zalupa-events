754ca63ac03ad2b1332e9e1f32b5f17f89213e9c
kauan <kauan@kauan>
2026-05-22 08:50:56 -0300

    telas novas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { Login } from './login/login';
+import { Home } from './home/home';
+import { Register } from './register/register';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: "", component: Login },
+    { path: "home", component: Home },
+    { path: "register", component: Register }
+];
